a0449fe7debc60c10988a841f5cb3ac6d32d4232
Rosnye <davd326at@gmail.com>
2024-05-21 20:09:44 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import PokeAPIComponent from './apiComponent'
+
+function App() {
+
+
+  return (
+    <body className='bg-cyan-200 min-h-screen flex flex-col items-center'>
+        <PokeAPIComponent/>
+    </body>
+  )
+}
+
+export default App
